b7786444a486a6113749478df9fa6072113e3392
Takafumi Arakaki <takafumi.a@gmail.com>
2021-01-22 13:46:46 -0800

    Reflect Julia 1.7-DEV's aotcompile.cpp in contrib/perfenv.sh (#20)

--- contrib/perfenv.sh
+++ contrib/perfenv.sh
@@ -24,14 +24,68 @@ set -x JL $JULIA_PATH/bin/julia$SUFFIX
 set -x OPT $LLVM_PATH/opt
 set -x OPTFLAGS -load=$JULIA_PATH/lib/libjulia$SUFFIX.so
 
-set -x JLPASSES -tbaa -PropagateJuliaAddrspaces -simplifycfg -dce -sroa -memcpyopt -always-inline -AllocOpt \
-          -instcombine -simplifycfg -sroa -instcombine -jump-threading -instcombine -reassociate \
-          -early-cse -AllocOpt -loop-idiom -loop-rotate -LowerSIMDLoop -licm -loop-unswitch \
-          -instcombine -indvars -loop-deletion -loop-unroll -AllocOpt -sroa -instcombine -gvn \
-          -memcpyopt -sccp -sink -instsimplify -instcombine -jump-threading -dse -AllocOpt \
-          -simplifycfg -loop-idiom -loop-deletion -jump-threading -slp-vectorizer -adce \
-          -instcombine -loop-vectorize -instcombine -barrier -LowerExcHandlers \
-          -GCInvariantVerifier -LateLowerGCFrame -dce -LowerPTLS -simplifycfg -CombineMulAdd
+# This list of pass should reflect `addOptimizationPasses`:
+# https://github.com/JuliaLang/julia/blob/ae53238c45a0cd6dafc6e121f4daaa93143bf627/src/aotcompile.cpp#L621-L806
+set -x JLPASSES \
+    --PropagateJuliaAddrspaces \
+    --scoped-noalias \
+    --tbaa \
+    --basic-aa \
+    --simplifycfg \
+    --dce \
+    --sroa \
+    --always-inline \
+    --AllocOpt \
+    --instcombine \
+    --simplifycfg \
+    --sroa \
+    --instsimplify \
+    --jump-threading \
+    --reassociate \
+    --early-cse \
+    --AllocOpt \
+    --loop-idiom \
+    --LowerSIMDLoop \
+    --licm \
+    --JuliaLICM \
+    --loop-unswitch \
+    --licm \
+    --JuliaLICM \
+    --instsimplify \
+    --indvars \
+    --loop-deletion \
+    --loop-unroll \
+    --AllocOpt \
+    --sroa \
+    --instsimplify \
+    --gvn \
+    --memcpyopt \
+    --sccp \
+    --instcombine \
+    --jump-threading \
+    --dse \
+    --AllocOpt \
+    --simplifycfg \
+    --loop-deletion \
+    --instcombine \
+    --loop-vectorize \
+    --loop-load-elim \
+    --simplifycfg \
+    --slp-vectorizer \
+    --adce \
+    --barrier \
+    --LowerExcHandlers \
+    --GCInvariantVerifier \
+    --LateLowerGCFrame \
+    --FinalLowerGC \
+    --gvn \
+    --sccp \
+    --dce \
+    --LowerPTLS \
+    --instcombine \
+    --simplifycfg \
+    --CombineMulAdd \
+    --div-rem-pairs
 
 # This one is a truncated pass pipeline I often use to debug the loop vectorizer
 set -x JLPASSES_UNTIL_LV -tbaa -PropagateJuliaAddrspaces -simplifycfg -dce -sroa -memcpyopt -always-inline -AllocOpt \
