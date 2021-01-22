b215f9fa422a2efffd242c8306e2f41c52e27476
Takafumi Arakaki <aka.tkf@gmail.com>
2021-01-22 15:10:02 -0500

    Reformat JLPASSES: one pass per line

--- contrib/perfenv.sh
+++ contrib/perfenv.sh
@@ -24,14 +24,62 @@ set -x JL $JULIA_PATH/bin/julia$SUFFIX
 set -x OPT $LLVM_PATH/opt
 set -x OPTFLAGS -load=$JULIA_PATH/lib/libjulia$SUFFIX.so
 
-set -x JLPASSES -tbaa -PropagateJuliaAddrspaces -simplifycfg -dce -sroa -memcpyopt -always-inline -AllocOpt \
-          -instcombine -simplifycfg -sroa -instcombine -jump-threading -instcombine -reassociate \
-          -early-cse -AllocOpt -loop-idiom -loop-rotate -LowerSIMDLoop -licm -loop-unswitch \
-          -instcombine -indvars -loop-deletion -loop-unroll -AllocOpt -sroa -instcombine -gvn \
-          -memcpyopt -sccp -sink -instsimplify -instcombine -jump-threading -dse -AllocOpt \
-          -simplifycfg -loop-idiom -loop-deletion -jump-threading -slp-vectorizer -adce \
-          -instcombine -loop-vectorize -instcombine -barrier -LowerExcHandlers \
-          -GCInvariantVerifier -LateLowerGCFrame -dce -LowerPTLS -simplifycfg -CombineMulAdd
+set -x JLPASSES \
+    -tbaa \
+    -PropagateJuliaAddrspaces \
+    -simplifycfg \
+    -dce \
+    -sroa \
+    -memcpyopt \
+    -always-inline \
+    -AllocOpt \
+    -instcombine \
+    -simplifycfg \
+    -sroa \
+    -instcombine \
+    -jump-threading \
+    -instcombine \
+    -reassociate \
+    -early-cse \
+    -AllocOpt \
+    -loop-idiom \
+    -loop-rotate \
+    -LowerSIMDLoop \
+    -licm \
+    -loop-unswitch \
+    -instcombine \
+    -indvars \
+    -loop-deletion \
+    -loop-unroll \
+    -AllocOpt \
+    -sroa \
+    -instcombine \
+    -gvn \
+    -memcpyopt \
+    -sccp \
+    -sink \
+    -instsimplify \
+    -instcombine \
+    -jump-threading \
+    -dse \
+    -AllocOpt \
+    -simplifycfg \
+    -loop-idiom \
+    -loop-deletion \
+    -jump-threading \
+    -slp-vectorizer \
+    -adce \
+    -instcombine \
+    -loop-vectorize \
+    -instcombine \
+    -barrier \
+    -LowerExcHandlers \
+    -GCInvariantVerifier \
+    -LateLowerGCFrame \
+    -dce \
+    -LowerPTLS \
+    -simplifycfg \
+    -CombineMulAdd
 
 # This one is a truncated pass pipeline I often use to debug the loop vectorizer
 set -x JLPASSES_UNTIL_LV -tbaa -PropagateJuliaAddrspaces -simplifycfg -dce -sroa -memcpyopt -always-inline -AllocOpt \
